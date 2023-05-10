36d62c93b7d2d5c09f8cd21562a27a1a43ac53fd
Leonardo Souza <101157679+lms-souza@users.noreply.github.com>
2023-05-09 23:33:04 -0300

    Add files via upload

---
+++ PastaPOO-03-05/aula12_ef_teste/Program.cs
@@ -0,0 +1,22 @@
+﻿﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+using Data;
+using Domain;
+
+using var db = new DataContext();
+
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new person");
+var person = new Person { Id = 1, Name = "João da Silva" };
+db.Add(person);
+db.SaveChanges();
+
+
+// Delete
+// Console.WriteLine("Delete the blog");
+// db.Remove(person);
+// db.SaveChanges();
---
+++ PastaPOO-03-05/aula12_ef_teste/data/DataContext.cs
@@ -0,0 +1,24 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public string DbPath { get; }
+
+        public DataContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "aula12_ef_teste.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
